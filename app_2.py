03d5914257028403b3fb69b7f53bbbc7f225033b
Dmitriy <30622552+DarkJoney@users.noreply.github.com>
2019-02-11 15:35:03 +0200

    Initial commit

---
+++ app_2.py
@@ -0,0 +1,31 @@
+import random
+
+person_counter = 0
+current_person = 0
+user_mode_input = ""
+is_waiting = True
+is_testing = False
+person_list = []
+correct_answers = []
+wrong_answer = []
+while True:
+
+    admin_name = "Vorobienko Petr Petrovich"
+    while is_waiting is True:
+        print("Hello, " + admin_name)
+        print("Please register person for testing: ")
+        person_list.append(input())
+        #append drugie soprovojdenie
+        print(person_list[person_counter] + " is registered")
+
+        is_waiting = False
+        is_testing = True
+
+    while is_testing is True:
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        testVal = random.randint(1, 4)
+        signs = ["*","/","+","-"]
+
+        print("What's the result of" + " " + str(a) + signs[testVal] + str(b) + "?")
+        userOtvet = input()
