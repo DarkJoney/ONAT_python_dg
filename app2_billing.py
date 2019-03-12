17b86edfff1d97eb9541bd2124113b0ee4cbf7e4
darkj <lest173@gmail.com>
2019-03-12 20:04:03 +0200

    2

--- app2_billing.py
+++ app2_billing.py
@@ -5,23 +5,27 @@ reg_data = {}
 billing_data = []
 
 
-def acc_stat():
-    pass
-
-def data_calls():
-    pass
-
 def ac_rev(phonen):
     if "money" in reg_data[phonen]:
+        print("Amount of avaliable money: " + str(reg_data[phonen]["money"]))
+    else:
+        print("The user didn't put any money on his account before")
+    if "payam" in reg_data[phonen]:
+        print("Latest payment was at " + str(reg_data[phonen]["data"]) + ", total num of payments: " + str(reg_data[phonen]["payam"]))
+    else:
+        print("The user didn't put any money on his account before")
 
 
+def ac_rep(phonen):
+    print("Data for: " + phonen)
+    if "money" in reg_data[phonen]:
+        print("Amount of avaliable money for calls " + str(reg_data[phonen]["money"]-4*reg_data[phonen]["numcall"]))
 
-def ac_rep():
-    pass
 
-
-def call_data():
-    pass
+def call_data(phonen):
+    print("Data for: " + phonen)
+    print("Last outcoming call: " + str(reg_data[phonen]["reccall"]) + " at " + str(reg_data[phonen]["lastrecdata"]))
+    print("Last incoming call: " + str(reg_data[phonen]["lastcall"]) + " at " + str(reg_data[phonen]["lastcalldata"]))
 
 
 def sub_data(usr_num):
@@ -44,7 +48,8 @@ def sub_reg():
 
     reg_data[phonen] = {
         "name": name,
-        "surname": surname
+        "surname": surname,
+        "numcall": 0
     }
     print(reg_data[phonen]["name"] + " is registered  in system")
 
@@ -54,7 +59,7 @@ def add_money(phonen):
     print("Enter amount of money:")
     money2add = int(input())
     if "money" in reg_data[phonen]:
-        reg_data[phonen]["money"] = reg_data[phonen["money"]] + money2add
+        reg_data[phonen]["money"] = reg_data[phonen]["money"] + money2add
         reg_data[phonen]["data"] = datetime.datetime.now()
         if "payAM" in reg_data[phonen]:
             reg_data[phonen]["payam"] = reg_data[phonen["payam"]] + 1
@@ -65,16 +70,19 @@ def add_money(phonen):
 
 
 def make_call(phonen):
-
+    reg_data[phonen]["numcall"] = int(reg_data[phonen]["numcall"]) + int(1)
     number = "0" + str(randint(630000000, 990000000))
     reg_data[phonen]["lastcall"] = number
-    print(reg_data[phonen]+" makes call to " + number)
+    reg_data[phonen]["lastcalldata"] = datetime.datetime.now()
+    print(phonen + " makes call to " + number)
+
 
 def rec_call(phonen):
 
     number = "0" + str(randint(630000000, 990000000))
     reg_data[phonen]["reccall"] = number
-    print(reg_data[phonen] + " gets call from " + number)
+    reg_data[phonen]["lastrecdata"] = datetime.datetime.now()
+    print(phonen + " gets call from " + number)
 
 while True:
     print("WELCOME TO SUBSCRIBER BILLING SYSTEM! What you would like to do?")
@@ -84,7 +92,6 @@ while True:
     print("3 - account review")
     print("4 - view data on calls")
     print("5 - account replenishment")
-    print("6 - status of the account")
     print("Additional Functions:")
     print("7 - add money to debit")
     print("8 - make a call")
@@ -101,11 +108,14 @@ while True:
         num = input()
         ac_rev(num)
     elif userInput is str("4"):
-        call_data()
+        print("Enter phone number: ")
+        num = input()
+        call_data(num)
     elif userInput is str("5"):
-        ac_rep()
-    elif userInput is str("6"):
-        acc_stat()
+        print("Enter phone number: ")
+        num = input()
+        ac_rep(num)
+
     elif userInput is str("7"):
         print("Enter phone number: ")
         num = input()
