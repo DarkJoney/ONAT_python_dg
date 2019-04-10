890bcbddad8de507f968514825b9e60627752f1d
DarkJoney <lest173@gmail.com>
2019-04-10 12:07:49 +0300

    Initial commit

--- excel/excel.py
+++ excel/excel.py
@@ -15,9 +15,9 @@ groups_id = []
 prevGroup = []
 data = sheet_1.cell(row=2, column=3).value
 for i in range(2, sheet_1.max_row):
-        if str(sheet_1.cell(row=i, column=3).value) not in groups_list:
-                groups_list.append(str(sheet_1.cell(row=i, column=3).value))
-                groups_id.append(i)
+    if str(sheet_1.cell(row=i, column=3).value) not in groups_list:
+        groups_list.append(str(sheet_1.cell(row=i, column=3).value))
+        groups_id.append(i)
 
 print(groups_list)
 print(groups_id)
@@ -25,45 +25,71 @@ print(groups_id)
 ws1 = wb.create_sheet("5.01")
 ws2 = wb.create_sheet("5.02")
 ws3 = wb.create_sheet("5.03")
-for i in range(1,7):
+for i in range(1, 7):
     ws1.cell(row=1, column=i).value = sheet_1.cell(row=1, column=i).value
-for i in range(1,7):
+for i in range(1, 7):
     ws2.cell(row=1, column=i).value = sheet_1.cell(row=1, column=i).value
-for i in range(1,7):
+for i in range(1, 7):
     ws3.cell(row=1, column=i).value = sheet_1.cell(row=1, column=i).value
 
 wb.save("students_edit.xlsx")
-for i in range(2,groups_id[1]-1):
-    ws1.cell(row=i, column=1).value = i-1
+for i in range(2, groups_id[1] - 1):
+    ws1.cell(row=i, column=1).value = i - 1
 
-
-
-for i in range(1,int(groups_id[1])-1):
-    for j in range(2,7):
+for i in range(1, int(groups_id[1]) - 1):
+    for j in range(2, 7):
         ws1.cell(row=i, column=j).value = sheet_1.cell(row=i, column=j).value
 wb.save("students_edit.xlsx")
 
-for i in range(2,groups_id[1]-1):
-    ws2.cell(row=i, column=1).value = i-1
+for i in range(2, groups_id[1] - 1):
+    ws2.cell(row=i, column=1).value = i - 1
 
-
-for i in range(int(groups_id[1]),int(groups_id[2])):
-    for j in range(2,7):
-        ws2.cell(row=i-6, column=j).value = sheet_1.cell(row=i, column=j).value
+for i in range(int(groups_id[1]), int(groups_id[2])):
+    for j in range(2, 7):
+        ws2.cell(row=i - 6, column=j).value = sheet_1.cell(row=i, column=j).value
 wb.save("students_edit.xlsx")
 
-for i in range(2,groups_id[1]-1):
-    ws3.cell(row=i, column=1).value = i-1
+for i in range(2, groups_id[1] - 1):
+    ws3.cell(row=i, column=1).value = i - 1
 
 for i in range(int(groups_id[2]), sheet_1.max_row):
-        for j in range(2, 7):
-            ws3.cell(row=i - 11, column=j).value = sheet_1.cell(row=i, column=j).value
+    for j in range(2, 7):
+        ws3.cell(row=i - 11, column=j).value = sheet_1.cell(row=i, column=j).value
 
+ws1.cell(row=1, column=7).value = "Mark"
+ws2.cell(row=1, column=7).value = "Mark"
+ws3.cell(row=1, column=7).value = "Mark"
 
-ws1.cell(row= 1, column=7).value = "Mark"
-ws2.cell(row= 1, column=7).value = "Mark"
-ws3.cell(row= 1, column=7).value = "Mark"
+value = 0
+for i in range(2, ws1.max_row + 1):
 
+    value = 0
+    for j in range(4, 7):
+        value = value + ws1.cell(row=i, column=j).value
+        print(value)
+        ws1.cell(row=i, column=7).value = round(value / 3)
 
+for i in range(2, ws2.max_row + 1):
+    value = 0
+    for j in range(4, 7):
+        value = value + ws2.cell(row=i, column=j).value
+        ws2.cell(row=i, column=7).value = round(value / 3)
+
+for i in range(2, ws3.max_row + 1):
+    value = 0
+    for j in range(4, 7):
+        value = value + ws3.cell(row=i, column=j).value
+        ws3.cell(row=i, column=7).value = round(value / 3)
 
 wb.save("students_edit.xlsx")
+
+print("SORTING...")
+
+temp = 0
+for i in range(2, ws3.max_row + 1):
+    temp = 0
+    if int(ws3.cell(row=i, column=7).value) > int(ws3.cell(row=i + 1, column=7).value):
+        temp = int(ws3.cell(row=i + 1, column=7).value)
+        ws3.cell(row=i + 1, column=7).value = ws3.cell(row=i, column=7).value
+        ws3.cell(row=i, column=7).value = temp
+        ws3.cell(
