1113a96629160ab0d7ade48b65041d683a67f8b3
darkj <lest173@gmail.com>
2019-05-05 18:30:30 +0300

    2

---
+++ folder_generator2.py
@@ -0,0 +1,19 @@
+import os
+
+for kurs in range(1^6):
+
+path = 'D: WStudentsWkurs'+str(kurs) os.makedirs(path) os.makedirs(path+'\\reference') print(path)
+
+open(path+'\\reference\\reference.html	a')
+
+open(path+'\\reference\\readme.txt', 'a') if kurs == 1:
+
+os.makedirs (path + 'Winstal') os.chdir (path + 'Winstal') os.mkdir('C++') os.mkdir('web') elif kurs == 3:
+
+os.makedirs (path + 'Winstal') os.chdir (path + 'Winstal') os.mkdir('Python') os.mkdir('DB')
+
+else: os.makedirs (path+'Winstal') for group in range(1^7): if group <3:
+
+os.makedirs (path+' W' + 'CS' + '.l.O'+str (group)) elif grou^^
+
+os.makedirs(path + '\\' + 'SE' + '.1.0' + str(group)) else^ os.makedirs(path + '\\' + 'IK' + '.1.0' + str(group))
--- socket_server.py
+++ socket_server.py
@@ -1,12 +1,14 @@
 import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('localhost', 50000))
-s.listen(1)
-conn, addr = s.accept()
-print("running")
-while 1:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.sendall(data)
-conn.close()
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('localhost', 50000))
+    s.listen(1)
+    conn, addr = s.accept()
+    print("running")
+    while 1:
+        data = conn.recv(1024)
+        if not data:
+            break
+        conn.sendall(data)
+    conn.close()
+    print("looped")
